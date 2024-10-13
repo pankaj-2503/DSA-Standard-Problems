e1af52f1b28a1b6c662e1c8b689a812a24842b29
pankaj-2503 <pankajpj3103@gmail.com>
2024-10-13 14:20:24 +0530

    dutch-algo

---
+++ Array/Dutch-national-flag-algo.cpp
@@ -0,0 +1,20 @@
+// Given an array consisting of only 0,1,2 sort it in time complexity of O(N) -> below algo is dutch-national-flag-algo
+// Time complexity - O(N)
+
+void sort012(vector<int>& arr) {
+    int low = 0, mid = 0;
+    int high = arr.size() - 1;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        } else if (arr[mid] == 1) {
+            mid++;
+        } else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
