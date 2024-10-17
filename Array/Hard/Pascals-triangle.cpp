31bf520c38bf34413940a3c84f6520a2fc9faef9
pankaj-2503 <pankajpj3103@gmail.com>
2024-10-17 22:58:08 +0530

    pascal

--- Array/Hard/Pascals-triangle.cpp
+++ Array/Hard/Pascals-triangle.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+
+// Recursive approach -> TC-O(N^2)
+vector<int> generatePascalTriangleRow(int row) {
+    if (row == 0) {
+        return {1}; // Base case: First row is always 1
+    }
+
+    vector<int> previousRow = generatePascalTriangleRow(row - 1);
+    vector<int> currentRow;
+
+    currentRow.push_back(1); // First element is always 1
+    for (int i = 1; i < previousRow.size(); i++) {
+        currentRow.push_back(previousRow[i - 1] + previousRow[i]);
+    }
+    currentRow.push_back(1); // Last element is always 1
+
+    return currentRow;
+}
+
 // TC: O(N^2) , SC: O(N)
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>ans;
