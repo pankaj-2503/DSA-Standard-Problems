79f7ccb6dbf6a148ef3e0dbdb1d7b793aa0ccfcc
pankaj-2503 <pankajpj3103@gmail.com>
2024-12-25 15:53:07 +0530

    first commit

--- Array/Hard/Majority-element2.cpp
+++ Array/Hard/Majority-element2.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-// TC: O(N) , sC: O(n)
+// TC: O(NlogN) , sC: O(n)
     vector<int> majorityElement(vector<int>& nums) {
         unordered_map<int,int>m;
         for(auto i:nums){
@@ -12,5 +12,47 @@ public:
             if(i.second>n/3) ans.push_back(i.first);
         }
         return ans;
+    }
+};
+
+// Using moore's voting algorithm 
+// TC: O(N)
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+          int candidate1 = 0, candidate2 = 1;  
+        int count1 = 0, count2 = 0;  
+
+        for (int num : nums) {  
+            if (num == candidate1) {  
+                count1++;  
+            } else if (num == candidate2) {  
+                count2++;  
+            } else if (count1 == 0) {  
+                candidate1 = num;  
+                count1 = 1;  
+            } else if (count2 == 0) {  
+                candidate2 = num;  
+                count2 = 1;  
+            } else {  
+                count1--;  
+                count2--;  
+            }  
+        }  
+        
+        // Step 2: Verify the candidates  
+        count1 = count2 = 0;  
+        for (int num : nums) {  
+            if (num == candidate1) count1++;  
+            else if (num == candidate2) count2++;  
+        }  
+
+        vector<int> result;  
+        int n = nums.size();  
+        if (count1 > n / 3) result.push_back(candidate1);  
+        if (count2 > n / 3) result.push_back(candidate2);  
+        
+        return result;  
     }
 };
