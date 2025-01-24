03bab58c960daf12779c4784d8c8b8b0b28218fd
pankaj-2503 <pankajpj3103@gmail.com>
2025-01-24 19:33:08 +0530

    tree

---
+++ Tree/CommonProblems/Invert-binary-tree.cpp
@@ -0,0 +1,18 @@
+// TC: O(n), SC: O(n)
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL) return NULL;
+        
+        TreeNode*temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+    
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
