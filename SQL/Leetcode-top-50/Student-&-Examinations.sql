831c76ac2ada7e6ecf2320ac1651d841c4a80521
pankaj-2503 <pankajpj3103@gmail.com>
2026-03-12 00:20:12 +0530

    sql-prac

---
+++ SQL/Leetcode-top-50/Student-&-Examinations.sql
@@ -0,0 +1,64 @@
+
+Input:
+Students table:
++------------+--------------+
+| student_id | student_name |
++------------+--------------+
+| 1          | Alice        |
+| 2          | Bob          |
+| 13         | John         |
+| 6          | Alex         |
++------------+--------------+
+Subjects table:
++--------------+
+| subject_name |
++--------------+
+| Math         |
+| Physics      |
+| Programming  |
++--------------+
+Examinations table:
++------------+--------------+
+| student_id | subject_name |
++------------+--------------+
+| 1          | Math         |
+| 1          | Physics      |
+| 1          | Programming  |
+| 2          | Programming  |
+| 1          | Physics      |
+| 1          | Math         |
+| 13         | Math         |
+| 13         | Programming  |
+| 13         | Physics      |
+| 2          | Math         |
+| 1          | Math         |
++------------+--------------+
+Output:
++------------+--------------+--------------+----------------+
+| student_id | student_name | subject_name | attended_exams |
++------------+--------------+--------------+----------------+
+| 1          | Alice        | Math         | 3              |
+| 1          | Alice        | Physics      | 2              |
+| 1          | Alice        | Programming  | 1              |
+| 2          | Bob          | Math         | 1              |
+| 2          | Bob          | Physics      | 0              |
+| 2          | Bob          | Programming  | 1              |
+| 6          | Alex         | Math         | 0              |
+| 6          | Alex         | Physics      | 0              |
+| 6          | Alex         | Programming  | 0              |
+| 13         | John         | Math         | 1              |
+| 13         | John         | Physics      | 1              |
+| 13         | John         | Programming  | 1              |
++------------+--------------+--------------+----------------+
+
+
+# Write your MySQL query statement below
+
+select s.student_id,s.student_name,sub.subject_name,count(e.subject_name) as attended_exams
+from students s
+cross join subjects sub
+left join examinations e
+on s.student_id=e.student_id and sub.subject_name=e.subject_name
+group by
+s.student_id,s.student_name,sub.subject_name
+order by s.student_id,sub.subject_name;
