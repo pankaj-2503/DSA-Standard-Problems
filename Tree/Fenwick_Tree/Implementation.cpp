48d8809e509f26e46ecdc9bd305c59bdbca4566c
pankaj-2503 <pankajpj3103@gmail.com>
2026-09-05 16:30:43 +0530

    inversion_cnt_fenwick_tree

--- Tree/Fenwick_Tree/Implementation.cpp
+++ Tree/Fenwick_Tree/Implementation.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+// Concepts
+
+
+// bit[i] -> stores the sum of range of actual array like [1,8],[2,4] , also as size of array could be greater then we need compression of index as array have limited size
+// what to find right most set bit -> i&-i  where -i is calculated using 2's complement which is 1+(~i), so unset the rightmost setbit we do x-=(x&-x)
+// here bit[i] calculates sum from range j+1 to i where j is i-(i&-i)
+// when we are building fenwick tree we are basically adding array part which is common for other ranges also , like which is contributing to other range eg. bit[1]=sum(1,4) , bit[8]=sum(4,8) then arr[4] is contributing to both partial_sort
+// for finding sum(1,13)= bit[13]+bit[12]+bit[8] as each one would be holding sum of range (13,13) , (9,12) , (1,8) respectively which is nothing but while i>0 , i-=(i&-i)
+// similarly for update we would go and update the part which is contributing to other range also using the same i+(i&-i)
+
+// TC -> update - O(logN) , sum - O(logn) ,build - O(NlogN)
+
 class FenwickTree{
     private:
         vector<long long>bit;
@@ -130,7 +142,7 @@ int main(){
 
 
 
-// using atcoder prebuilt function #include<atcoder/all>
+// using atcoder prebuilt function #include<atcoder/all>l  , but it won't work on online compiler like codeforces so use full implementation
 
 
 #include <atcoder/all> // Include ACL's Fenwick Tree
@@ -177,3 +189,49 @@ int main() {
 
     return 0;
 }
+
+
+
+
+// Inversion count  - Given an array of integers arr[]. You have to find the Inversion Count of the array. Inversion count is the number of pairs of elements (i, j) such that i < j and arr[i] > arr[j].
+
+class ft{
+    private:
+    vector<int>bit;
+    int n;
+    public:
+     ft(int size=0):n(size),bit(size+1,0){}
+
+     void update(int i,int delta){
+         for(;i<=n;i+=(i&-i)) bit[i]+=delta;
+     }
+     int sum(int i){
+         int ans=0;
+         for(;i>0;i-=(i&-i)) ans+=bit[i];
+         return ans;
+     }
+};
+class Solution {
+  public:
+
+    int inversionCount(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+
+        //index compression
+        vector<int>temp=arr;
+        sort(temp.begin(),temp.end());
+        for(int i=0;i<n;i++){
+            arr[i]=lower_bound(temp.begin(),temp.end(),arr[i])-temp.begin()+1; // as fenwick tree array bit is 1 based
+        }
+
+        ft f(n+1);
+        int cnt=0;
+        for(int i=n-1;i>=0;i--){
+            cnt+=f.sum(arr[i]-1);
+            f.update(arr[i],1);
+        }
+        return cnt;
+
+    }
+};
